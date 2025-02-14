e2236f74701071f402c5c2f60ebe4111ab51d05b
Dobbymin <gmin990624@gmail.com>
2025-02-14 16:30:53 +0900

    style: 공통 스타일 코드 작성

---
+++ .stylelintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-standard-scss',
+    'stylelint-config-prettier-scss',
+    'stylelint-config-recommended-scss',
+    'stylelint-config-property-sort-order-smacss',
+  ],
+  plugins: ['stylelint-scss', 'stylelint-order'],
+  rules: {
+    'selector-class-pattern': null,
+  },
+};
