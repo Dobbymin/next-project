e2901d4ab099c8c3301f528c6d6d5cd5d3d5c477
Dobbymin <gmin990624@gmail.com>
2025-02-14 16:32:01 +0900

    feat: 사용하는 components 파일 전부 작성

--- src/app/(afterLogin)/_components/index.ts
+++ src/app/(afterLogin)/_components/index.ts
@@ -1,1 +1,6 @@
 export * from './search-form';
+export * from './back-button';
+export * from './follow-recommend';
+export * from './logout-button';
+export * from './right-search-zone';
+export * from './search-form';
